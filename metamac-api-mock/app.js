99e4e2b2cae6774d8fe096029fd30f39577524d3
aherfer <aherfer@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-07-29 07:05:41 +0000

    Mock de la api
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-portal/trunk/metamac-portal@48145 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-api-mock/app.js
@@ -0,0 +1,32 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var api = require('./routes/api');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', 8080);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}
+
+app.get('/metamac-statistical-resources-external-web/apis/statistical-resources/v1.0/datasets/ISTAC/C00025A_000001/001.000', api.dataset);
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
