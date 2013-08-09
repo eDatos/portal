d832f2dfcd0274941c2739dee91f54a8fb035794
aherfer <aherfer@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-08-09 08:57:35 +0000

    Level filter style
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-portal/trunk/metamac-portal@48526 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-portal-web/src/main/webapp/WEB-INF/theme/js/metamac/views/HandlebarsTemplates.js
+++ metamac-portal-web/src/main/webapp/WEB-INF/theme/js/metamac/views/HandlebarsTemplates.js
@@ -2274,7 +2274,7 @@ function program3(depth0,data) {
     + "\n        </div>\n    </div>\n\n    <div class=\"collapse ";
   stack2 = helpers.unless.call(depth0, ((stack1 = depth0.dimension),stack1 == null || stack1 === false ? stack1 : stack1.open), {hash:{},inverse:self.noop,fn:self.program(3, program3, data),data:data});
   if(stack2 || stack2 === 0) { buffer += stack2; }
-  buffer += "\" >\n        <div class=\"filter-sidebar-dimension-searchbar\"></div>\n        <div class=\"filter-sidebar-dimension-levels\"></div>\n        <div class=\"filter-sidebar-dimension-actions\"></div>\n        <div class=\"filter-sidebar-categories\"></div>\n    </div>\n\n</div>";
+  buffer += "\" >\n        <div class=\"filter-sidebar-dimension-searchbar\"></div>\n\n        <div class=\"filter-sidebar-dimension-actionsbar\">\n            <div class=\"filter-sidebar-dimension-levels\"></div>\n            <div class=\"filter-sidebar-dimension-actions\"></div>\n        </div>\n\n        <div class=\"filter-sidebar-categories\"></div>\n    </div>\n\n</div>";
   return buffer;
   });
 
