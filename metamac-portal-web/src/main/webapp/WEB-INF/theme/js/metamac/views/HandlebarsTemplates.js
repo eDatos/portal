04b46cc91b10bd1b583ec405d18119d76f218334
aherfer <aherfer@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-08-12 14:11:55 +0000

    Collection description
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-portal/trunk/metamac-portal@48589 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-portal-web/src/main/webapp/WEB-INF/theme/js/metamac/views/HandlebarsTemplates.js
+++ metamac-portal-web/src/main/webapp/WEB-INF/theme/js/metamac/views/HandlebarsTemplates.js
@@ -104,14 +104,27 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 this["Handlebars"]["templates"]["collection/collection"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression, self=this;
 
+function program1(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\n<p>";
+  if (stack1 = helpers.description) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.description; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "</p>\n";
+  return buffer;
+  }
 
   buffer += "<h3>";
   if (stack1 = helpers.name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.name; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "</h3>\n\n<div class=\"collection-nodes\">\n</div>";
+    + "</h3>\n\n";
+  stack1 = helpers['if'].call(depth0, depth0.description, {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n\n<div class=\"collection-nodes\">\n</div>";
   return buffer;
   });
 
