afbb3958660ac3c296d5f8e3b71cf38080e38f9c
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-09-12 10:48:45 +0000

    In progress: crear proyecto api
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-portal/trunk/metamac-portal@49327 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-portal-rest-external-api/src/main/java/org/siemac/metamac/rest/portal/v1_0/domain/package-info.java
@@ -0,0 +1,3 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.siemac.org/metamac/rest/portal/v1.0/domain", xmlns = {
+        @javax.xml.bind.annotation.XmlNs(namespaceURI = "http://www.siemac.org/metamac/rest/portal/v1.0/domain", prefix = "portal")})
+package org.siemac.metamac.rest.portal.v1_0.domain;
