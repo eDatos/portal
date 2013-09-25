10d090479acc202509ba5894a597df98402ee695
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-09-25 10:43:47 +0000

    Bean propio para configuración
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-portal/trunk/metamac-portal@49568 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-portal-core/src/main/java/org/siemac/metamac/portal/core/conf/PortalConfiguration.java
+++ metamac-portal-core/src/main/java/org/siemac/metamac/portal/core/conf/PortalConfiguration.java
@@ -1,22 +1,7 @@
 package org.siemac.metamac.portal.core.conf;
 
-import java.util.List;
-
 import org.siemac.metamac.core.common.conf.ConfigurationService;
-import org.siemac.metamac.core.common.exception.MetamacException;
 
 public interface PortalConfiguration extends ConfigurationService {
-
-    /**
-     * Retrieves all languages managed by service
-     */
-    @Override
-    public List<String> retrieveLanguages() throws MetamacException;
-
-    /**
-     * Retrieves language as default
-     */
-    @Override
-    public String retrieveLanguageDefault() throws MetamacException;
 
 }
