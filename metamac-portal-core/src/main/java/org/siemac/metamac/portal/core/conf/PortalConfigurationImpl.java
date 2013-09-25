64b0430decbfde00dc79ed3c8d41d86ea48e1424
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-09-25 07:31:01 +0000

    Bean propio para configuración
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-portal/trunk/metamac-portal@49565 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-portal-core/src/main/java/org/siemac/metamac/portal/core/conf/PortalConfigurationImpl.java
@@ -0,0 +1,7 @@
+package org.siemac.metamac.portal.core.conf;
+
+import org.siemac.metamac.core.common.conf.ConfigurationServiceImpl;
+
+public class PortalConfigurationImpl extends ConfigurationServiceImpl implements PortalConfiguration {
+
+}
