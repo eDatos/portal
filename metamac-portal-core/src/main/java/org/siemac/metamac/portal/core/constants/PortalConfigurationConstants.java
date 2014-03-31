c84fec359abb321c282246576b7c58a49ab40695
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-03-31 10:25:37 +0000

    METAMAC-2214 - [API] Cambiar el captcha de Google por el de Gobierno
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-portal/trunk/metamac-portal@51804 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-portal-core/src/main/java/org/siemac/metamac/portal/core/constants/PortalConfigurationConstants.java
+++ metamac-portal-core/src/main/java/org/siemac/metamac/portal/core/constants/PortalConfigurationConstants.java
@@ -5,13 +5,18 @@ import org.siemac.metamac.core.common.constants.shared.ConfigurationConstants;
 public class PortalConfigurationConstants extends ConfigurationConstants {
 
     // DataSource
-    public static final String DB_URL         = "metamac.portal.db.url";
-    public static final String DB_USERNAME    = "metamac.portal.db.username";
-    public static final String DB_PASSWORD    = "metamac.portal.db.password";
-    public static final String DB_DIALECT     = "metamac.portal.db.dialect";
-    public static final String DB_DRIVER_NAME = "metamac.portal.db.driver_name";
+    public static final String DB_URL                     = "metamac.portal.db.url";
+    public static final String DB_USERNAME                = "metamac.portal.db.username";
+    public static final String DB_PASSWORD                = "metamac.portal.db.password";
+    public static final String DB_DIALECT                 = "metamac.portal.db.dialect";
+    public static final String DB_DRIVER_NAME             = "metamac.portal.db.driver_name";
 
-    public static final String CAPTCHA_ENABLE = "metamac.portal.rest.external.authentication.captcha.enable";
-    public static final String CAPTCHA_PUBLIC_KEY = "metamac.portal.rest.external.authentication.captcha.public_key";
-    public static final String CAPTCHA_PRIVATE_KEY = "metamac.portal.rest.external.authentication.captcha.private_key";
+    public static final String CAPTCHA_PROVIDER           = "metamac.portal.rest.external.authentication.captcha.provider";
+    public static final String CAPTCHA_ENABLE             = "metamac.portal.rest.external.authentication.captcha.enable";
+    public static final String CAPTCHA_PUBLIC_KEY         = "metamac.portal.rest.external.authentication.captcha.public_key";
+    public static final String CAPTCHA_PRIVATE_KEY        = "metamac.portal.rest.external.authentication.captcha.private_key";
+
+    public static final String CAPTCHA_PROVIDER_GOBCAN    = "gobcan";
+    public static final String CAPTCHA_PROVIDER_RECAPTCHA = "recaptcha";
+    public static final String CAPTCHA_PROVIDER_SIMPLE    = "simple";
 }
--- metamac-portal-rest-external-impl/src/main/java/org/siemac/metamac/portal/rest/external/authentication/CaptchaFilter.java
+++ metamac-portal-rest-external-impl/src/main/java/org/siemac/metamac/portal/rest/external/authentication/CaptchaFilter.java
@@ -2,6 +2,8 @@ package org.siemac.metamac.portal.rest.external.authentication;
 
 import net.tanesha.recaptcha.ReCaptchaImpl;
 import net.tanesha.recaptcha.ReCaptchaResponse;
+import nl.captcha.Captcha;
+
 import org.apache.cxf.jaxrs.ext.RequestHandler;
 import org.apache.cxf.jaxrs.impl.HttpHeadersImpl;
 import org.apache.cxf.jaxrs.model.ClassResourceInfo;
@@ -19,7 +21,7 @@ import java.util.List;
 public class CaptchaFilter implements RequestHandler {
 
     @Context
-    private HttpServletRequest request;
+    private HttpServletRequest  request;
 
     @Autowired
     private PortalConfiguration portalConfiguration;
@@ -35,6 +37,7 @@ public class CaptchaFilter implements RequestHandler {
     @Override
     public Response handleRequest(Message m, ClassResourceInfo resourceClass) {
         boolean captchaEnabled = portalConfiguration.getConfig().getBoolean(PortalConfigurationConstants.CAPTCHA_ENABLE);
+        String captchaProvider = portalConfiguration.getConfig().getString(PortalConfigurationConstants.CAPTCHA_PROVIDER);
 
         if (!captchaEnabled) {
             return null;
@@ -42,19 +45,12 @@ public class CaptchaFilter implements RequestHandler {
 
         if ("POST".equals(request.getMethod())) {
             boolean valid = false;
-
-            HttpHeaders headers = new HttpHeadersImpl(m);
-            String challenge = getSingleHeader(headers, "recaptcha_challenge");
-            String response = getSingleHeader(headers, "recaptcha_response");
-
-            if (challenge != null && response != null) {
-                String remoteAddr = request.getRemoteAddr();
-
-                ReCaptchaImpl reCaptcha = new ReCaptchaImpl();
-                reCaptcha.setPrivateKey(portalConfiguration.getProperty(PortalConfigurationConstants.CAPTCHA_PRIVATE_KEY));
-                ReCaptchaResponse reCaptchaResponse = reCaptcha.checkAnswer(remoteAddr, challenge, response);
-
-                valid = reCaptchaResponse.isValid();
+            if (PortalConfigurationConstants.CAPTCHA_PROVIDER_GOBCAN.equals(captchaProvider)) {
+                valid = validateCaptchaGobcan(m);
+            } else if (PortalConfigurationConstants.CAPTCHA_PROVIDER_RECAPTCHA.equals(captchaProvider)) {
+                valid = validateRecaptchaGobcan(m);
+            } else if (PortalConfigurationConstants.CAPTCHA_PROVIDER_SIMPLE.equals(captchaProvider)) {
+                valid = validateSimple(m);
             }
 
             if (!valid) {
@@ -62,8 +58,52 @@ public class CaptchaFilter implements RequestHandler {
             }
         }
 
-
         return null;
     }
 
+    private boolean validateSimple(Message m) {
+        HttpHeaders headers = new HttpHeadersImpl(m);
+        Object simpleCaptchaAnswer = request.getSession().getAttribute(Captcha.NAME);
+        String responseSimple = getSingleHeader(headers, "captcha_simple_response");
+
+        boolean valid = false;
+        if (responseSimple != null) {
+            if (simpleCaptchaAnswer != null) {
+                Captcha captcha = (Captcha) simpleCaptchaAnswer;
+                valid = captcha.isCorrect(responseSimple);
+            }
+        }
+        return valid;
+    }
+
+    private boolean validateRecaptchaGobcan(Message m) {
+        HttpHeaders headers = new HttpHeadersImpl(m);
+        String challengeRecaptcha = getSingleHeader(headers, "recaptcha_challenge");
+        String responseRecaptcha = getSingleHeader(headers, "recaptcha_response");
+
+        boolean valid = false;
+
+        if (challengeRecaptcha != null && responseRecaptcha != null) {
+            String remoteAddr = request.getRemoteAddr();
+
+            ReCaptchaImpl reCaptcha = new ReCaptchaImpl();
+            reCaptcha.setPrivateKey(portalConfiguration.getProperty(PortalConfigurationConstants.CAPTCHA_PRIVATE_KEY));
+            ReCaptchaResponse reCaptchaResponse = reCaptcha.checkAnswer(remoteAddr, challengeRecaptcha, responseRecaptcha);
+
+            valid = reCaptchaResponse.isValid();
+        }
+        return valid;
+    }
+
+    private boolean validateCaptchaGobcan(Message m) {
+        HttpHeaders headers = new HttpHeadersImpl(m);
+        Object gobcanCaptchaAnswer = request.getSession().getAttribute("captcha_gobcan");
+        String responseGobcan = getSingleHeader(headers, "captcha_gobcan");
+
+        boolean valid = false;
+        if (gobcanCaptchaAnswer != null) {
+            valid = gobcanCaptchaAnswer.toString().equals(responseGobcan);
+        }
+        return valid;
+    }
 }
