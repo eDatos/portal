955be56ff7ca07101889b7003886f267739611f8
acidcar <acidcar@arte-consultores.com>
2016-09-26 12:30:08 +0100

    METAMAC-2506 - Automatizar la creación de los paquetes de instalación > Igualando rutas

---
+++ etc/db/portal/oracle/02-drop/01-portal-drop.sql
@@ -0,0 +1,16 @@
+
+    
+-- ###########################################
+-- # Drop
+-- ###########################################
+-- Drop index
+
+
+-- Drop many to many relations
+    
+-- Drop normal entities
+    
+DROP TABLE TB_PERMALINKS CASCADE CONSTRAINTS PURGE;
+
+
+-- Drop pk sequence
