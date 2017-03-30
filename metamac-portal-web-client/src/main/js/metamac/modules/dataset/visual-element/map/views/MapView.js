210659ff5df9418263a74333b4bc7fb5bd1ec0b2
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2017-03-30 12:50:27 +0100

    METAMAC-2032 - [Página 3] Añadir mapa de símbolos > Limpiando código comentado asociado a la antigua implementación de d3

--- metamac-portal-web-client/src/main/js/metamac/modules/dataset/visual-element/map/views/MapView.js
+++ metamac-portal-web-client/src/main/js/metamac/modules/dataset/visual-element/map/views/MapView.js
@@ -107,30 +107,11 @@
         },
 
         render : function () {
-            // this._calculateOriginAndScale();
-            // this._createProjection();
-            // this._createSvgContainerAndTooltip();            
-            
-            // this._legendView = new App.Map.LegendView({el : this._svg[0], model : this.model, container : this.el });
-            // this._legendView.render();
-
             this._createMapContent();
-
-            // this._creditsView = new App.Map.CredistView({ el : this._svg[0], container : this.el });
-            // this._creditsView.render();
-
-            // this.model.set({currentScale : 2});
-
             this.delegateEvents();
         },
 
         destroy : function () {
-            // if (this._svg) {
-            //     this._svg.remove();
-            // }
-            // if (this.tooltip) {
-            //     this.tooltip.destroy();
-            // }
             if (this.map) {
                 this.map.destroy();
             }
@@ -144,14 +125,6 @@
             var x = this.model.get("x");
             var y = this.model.get("y");
             var animationDelay = this.model.get("animationDelay");
-            // if (animationDelay) {
-            //     this._regions.transition()
-            //         .duration(animationDelay)
-            //         .attr("transform", "scale(" + currentScale + ")translate(" + x + "," + y + ")");
-            // } else {
-            //     this._regions.attr("transform", "scale(" + currentScale + ")translate(" + x + "," + y + ")");
-            // }
-            // console.log(oldScale, currentScale, x, y)
             this.map.mapZoom(oldScale / currentScale);
         },
 
@@ -160,35 +133,16 @@
         },
 
         zoomExit : function () {
-            // this._centered = null;
-            // this._regions.selectAll("path")
-            //     .classed("active", this._centered);
             this._centerAndZoom();
         },
 
         updateRanges : function () {
-            // this._legendView.render();
             this._updateDataClasses();
-            // this._repaintRegions();
         },
 
         canRender : function () {
             return this._shapeListOrderByHierarchy().length > 0;
         },
-
-        // _updateSize : function () {
-        //     if (this._svg) {
-        //         var newWidth = this.$el.width();
-        //         var newHeight = this.$el.height();
-
-        //         if (newWidth !== this._width || newHeight !== this._height) {
-        //             this._width = newWidth;
-        //             this._height = newHeight;
-        //             this.destroy();
-        //             this.render();
-        //         }
-        //     }
-        // },
 
         _calculateOriginAndScale : function () {
             this._origin = this._calculateNewOriginAndBounds();
@@ -213,64 +167,7 @@
             var originX = this._minX + (this._maxX - this._minX) / 2;
             var originY = this._minY + (this._maxY - this._minY) / 2;
             return [originX, originY];
-        },
-
-        //_calculateNewScale : function (coordinates, pixels, origin) {
-            // var tempProjection = d3.geo.albers()
-            //     .origin(origin)
-            //     .translate([0, 0]);
-
-            // var currentScale = tempProjection.scale();
-            // var currentValX = tempProjection([coordinates.x, coordinates.y])[0];
-            // var newScaleX = ((-this._width / 2) * currentScale) / currentValX;
-
-            // var currentValY = tempProjection([coordinates.x, coordinates.y])[1];
-            // var newScaleY = ((-this._height / 2) * currentScale) / currentValY;
-
-            // if (Math.abs(newScaleX) < Math.abs(newScaleY)) {
-            //     return Math.abs(newScaleX) / 2.3;
-            // } else {
-            //     return Math.abs(newScaleY) / 2.3;
-            // }
-        //},
-
-        // _createProjection : function () {
-        //     this._projection = d3.geo.albers()
-        //         .origin(this._origin)
-        //         .scale(this._scale)
-        //         .translate([0, 0]);
-
-        //     this._path = d3.geo.path()
-        //         .projection(this._projection);
-
-        //     // TODO: The events should be managed by the manager
-        //     this._dnd = d3.behavior.drag();
-        //     this._dnd.on("drag", _.bind(this._handleDrag, this));
-        //     this._dnd.on('dragstart', _.bind(this._handleDragstart, this));
-        //     this._dnd.on('dragend', _.bind(this._handleDragend, this));
-        // },
-
-        // _createSvgContainerAndTooltip : function () {
-        //     this._svg = d3.select($('#highmaps-container-2')[0]).append("svg:svg")
-        //         .attr("width", this._width)
-        //         .attr("height", this._height)
-        //         .attr("xmlns", "http://www.w3.org/2000/svg")
-        //         .attr("version", "1.1")
-        //         .attr('class', "map");
-
-        //     this._svg.append("rect")
-        //         .attr("class", "background")
-        //         .attr("width", this._width)
-        //         .attr("height", this._height)
-        //         .on("click", _.bind(this._handleClick, this))
-        //         .call(this._dnd);
-
-        //     if (this.tooltip) {
-        //         this.tooltip.destroy();
-        //     }
-
-        //     this.tooltip = new App.components.tooltip.Tooltip({el : this.el, delegate : this.tooltipDelegate});
-        // },
+        },      
 
         _shapeListOrderByHierarchy : function () {
             return _.chain(this._shapeList).compact().sortBy(function (shape) {
@@ -279,20 +176,8 @@
         },
 
         _createMapContent : function () {
-            // var self = this;
-
-            // this._width = this.$el.width();
-            // this._height = this.$el.height();
-
-            // this._regions = this._svg.append("svg:g")
-            //     .attr("transform", "translate(" + this._width / 2 + "," + this._height / 2 + ")")
-            //     .append("g")
-            //     .attr("id", "regions")
-            //     .call(this._dnd);
-            
-
-            var geoJson = GeoJsonConverter.shapeListToGeoJson(this._shapeListOrderByHierarchy());
-            // console.log("geojson", geoJson);
+          
+            var geoJson = GeoJsonConverter.shapeListToGeoJson(this._shapeListOrderByHierarchy());          
 
             var containerGeoJson = GeoJsonConverter.shapeListToGeoJson([this._container], {contour : true});
             var features = _.union(containerGeoJson.features, geoJson.features);            
@@ -301,9 +186,6 @@
             var containerMapData = this._getMapDataFromGeoJson(containerGeoJson);
 
             var data = this._getData();
-            // console.log("mapdata", mapData);
-            // console.log("data", data);
-            // console.log("containermapdata", containerMapData);
 
             var worldContainerSerie = _.defaults({
                     id : "worldContainerSerie", 
@@ -366,22 +248,10 @@
             this._defaultMapOptions.colorAxis.dataClasses = this._generateDataClasses();
 
             this._defaultMapOptions.credits.text = this._dataset ? this._dataset.metadata.getRightsHolder().name : '';
-//            this._defaultMapOptions.credits.href = this._dataset ? this._dataset.metadata.getRightsHolder().href : '';
-
 
             this.map = new Highmaps.Map(this._defaultMapOptions);
 
             this._centerAndZoom();
-
-            // // Regions
-            // this._regions.selectAll("path")
-            //     .data(features)
-            //     .enter().append("svg:path")
-            //     .attr("d", this._path)
-            //     // .attr("class", _.bind(this._calculateColor, this))
-            //     .on("click", _.bind(self._handleClick, this))
-            //     .on("mouseover", self.tooltipDelegate.mouseOver)
-            //     .on("mouseout", self.tooltipDelegate.mouseOut);
         },
 
         _getData : function() {
@@ -403,7 +273,6 @@
         _updateDataClasses : function() {
             if (this.map) {
                 var axis = this.map.colorAxis[0];
-                console.log(axis.getExtremes());
                  if (this.model.get('mapType') == 'map') {
                     axis.update({
                         dataClasses: this._generateDataClasses()
@@ -462,96 +331,6 @@
             this.map.legend.render();
         },
 
-        // _repaintRegions : function () {
-        //     var self = this;
-        //     this._regions.selectAll("path")
-        //         // .attr("class", self._calculateColor)
-        //         .classed("active", function (d) {
-        //             return d === self._centered;
-        //         });
-        // },
-
-        // _calculateColor : function (d) {
-        //     if (d.id && !d.properties.contour) {
-        //         var normCodeData = this._dataJson[d.id];
-        //         if (normCodeData) {
-        //             var value = normCodeData.value;
-        //             if (!isNaN(value)) {
-        //                 var currentRangesNum = this.model.get("currentRangesNum");
-        //                 var color = "color" + currentRangesNum + '-' + this.model.quantizer(value);
-        //                 return color;
-        //             }
-        //         } else {
-        //             return "noAvailable";
-        //         }
-        //     } else if (d.properties.contour) {
-        //         return "contour";
-        //     }
-        // },
-
-        // _newCenterOffset : function (pathBounds) {
-        //     var origin = [(pathBounds[0][0] + pathBounds[1][0]) / 2, (pathBounds[0][1] + pathBounds[1][1]) / 2];
-        //     var originPx = this._projection(origin);
-        //     originPx[0] *= -1;
-        //     originPx[1] *= -1;
-        //     return originPx;
-        // },
-
-        // _newScaleCenteredInObject : function (pathBounds) {
-        //     var origin = [(pathBounds[0][0] + pathBounds[1][0]) / 2, (pathBounds[0][1] + pathBounds[1][1]) / 2];
-        //     var leftBottomCoordinates = {x : pathBounds[0][0], y : pathBounds[0][1]};
-        //     var pixels = {x : 0, y : this._height};
-        //     var totalNewScale = this._calculateNewScale(leftBottomCoordinates, pixels, origin);
-        //     var newScaleFactor = totalNewScale / this._scale;
-        //     newScaleFactor = this.model.transformToValidScaleFactor(newScaleFactor);
-        //     return newScaleFactor;
-        // },
-
-        // _handleClick : function (d) {
-        //     var self = this;
-        //     var maxScale = this.model.get("maxScale");
-        //     var minScale = this.model.get("minScale");
-        //     var originPx = [0, 0],
-        //         currentScale = minScale,
-        //         action = true;
-        //     //TODO: Do we know that there is always going to be a "normCode" property?
-        //     if (d && d.properties.normCode && !d.properties.contour) {
-        //         this._centered = d;
-        //         this._regions.selectAll("path")
-        //             .classed("active", this._centered && function (d) {
-        //                 return d === self._centered;
-        //             });
-        //         // Getting the new origin and scale
-        //         var pathBounds = d3.geo.bounds(d);
-        //         originPx = this._newCenterOffset(pathBounds);
-        //         currentScale = this._newScaleCenteredInObject(pathBounds);
-        //         this.model.set({currentScale : currentScale, x : originPx[0], y : originPx[1], animationDelay : 1000});
-        //     } else {
-        //         this._centered = null;
-        //         this._regions.selectAll("path").classed("active", false);
-        //     }
-        // },
-
-        // _handleDrag : function (d) {
-        //     var x = this.model.get("x");
-        //     var y = this.model.get("y");
-        //     x += this.model.scaleMovement(d3.event.dx);
-        //     y += this.model.scaleMovement(d3.event.dy);
-
-        //     this.model.set({x : x, y : y, animationDelay : 0});
-        //     return true;
-        // },
-
-        // _handleDragstart : function () {
-        //     this.$el.addClass('grabbing');
-        //     return true;
-        // },
-
-        // _handleDragend : function () {
-        //     this.$el.removeClass('grabbing');
-        //     return true;
-        // },
-
         _offsetFromMouseEvent : function (e) {
             var offset = this.$el.offset();
             var xOffset = e.pageX - (offset.left + this._width / 2);
@@ -571,8 +350,6 @@
         },
 
         _onResize : function () {
-            // this._updateSize();
-            // this.tooltip.setEl(this.el);
             this.map.reflow();
         }
 
