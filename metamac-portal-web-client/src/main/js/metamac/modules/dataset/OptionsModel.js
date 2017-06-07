008b87632f6ec4fb4f1f69f9c734a8625653691c
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2017-06-07 15:35:15 +0100

    METAMAC-2608 - Permitir usar el visualizador para visualizar indicadores > Autoformatting

--- metamac-portal-web-client/src/main/js/metamac/modules/dataset/OptionsModel.js
+++ metamac-portal-web-client/src/main/js/metamac/modules/dataset/OptionsModel.js
@@ -1,19 +1,19 @@
-(function() {
+(function () {
     App.namespace("App.modules.dataset");
-    
+
     App.modules.dataset.OptionsModel = Backbone.Model.extend({
-        defaults : {
-            type : '',
-            fullScreen : false,
-            filter : false,
-            visualize : false,
-            options : true,
-            widget : false,
-            widgetButton : true,
-            widgetInitialType : '',
+        defaults: {
+            type: '',
+            fullScreen: false,
+            filter: false,
+            visualize: false,
+            options: true,
+            widget: false,
+            widgetButton: true,
+            widgetInitialType: '',
             menu: true,
             downloadButton: true
         }
     });
-    
+
 })();
