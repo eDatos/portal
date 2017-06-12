c207c1ec0a726f1b1b5a4d49d01d0a05300d56c8
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2017-06-12 13:58:44 +0100

    NONE - Autoformatting

--- metamac-portal-web-client/src/main/js/metamac/modules/dataset/visual-element/Base.js
+++ metamac-portal-web-client/src/main/js/metamac/modules/dataset/visual-element/Base.js
@@ -10,7 +10,7 @@
 
     App.VisualElement.Base.prototype = {
 
-        initialize : function (options) {
+        initialize: function (options) {
             options = options || {};
             this.dataset = options.dataset;
 
@@ -22,7 +22,7 @@
             this._initializeChartOptions();
         },
 
-        getFixedPermutation : function () {
+        getFixedPermutation: function () {
             var fixedPermutation = {};
             var self = this;
             this.filterDimensions.getAllFixedDimensionsCopy().forEach(function (dimension) {
@@ -32,67 +32,69 @@
             return fixedPermutation;
         },
 
-        getTitle : function () {
+        getTitle: function () {
             var self = this;
-            var fixedLabels = this.filterDimensions.getAllFixedDimensionsCopy().map(function (dimension) {
-                var selectedRepresentations = self.getDrawableRepresentations(dimension);
-                return selectedRepresentations[0].get('visibleLabel') + ".";
-            });
+            var fixedLabels = this.filterDimensions
+                .getAllFixedDimensionsCopy()
+                .map(function (dimension) {
+                    var selectedRepresentations = self.getDrawableRepresentations(dimension);
+                    return selectedRepresentations[0].get('visibleLabel') + ".";
+                });
             return fixedLabels.length ? fixedLabels.join(" ") : "";
         },
 
-        load : function () {
+        load: function () {
         },
 
-        destroy : function () {
+        destroy: function () {
         },
 
-        updatingDimensionPositions : function () {
+        updatingDimensionPositions: function () {
         },
 
-        resizeFullScreen : function () {
+        resizeFullScreen: function () {
             this.destroy();
             this.load();
         },
 
-        _initializeChartOptions : function () {            
+        _initializeChartOptions: function () {
             this._chartOptions = {
-                title : {
-                    text : null
+                title: {
+                    text: null
                 },
-                credits : {
-                    position : {
-                        y : 0,
-                        x : -20
+                credits: {
+                    position: {
+                        y: 0,
+                        x: -20
                     }
                 },
-                legend : {
-                    layout : 'horizontal',
-                    backgroundColor : Constants.colors.istacWhite,
-                    x : 5,
-                    y : 0,
-                    borderWidth : 1,
+                legend: {
+                    layout: 'horizontal',
+                    backgroundColor: Constants.colors.istacWhite,
+                    x: 5,
+                    y: 0,
+                    borderWidth: 1,
                     borderColor: Constants.colors.istacGreyMedium,
-                    floating : false,
-                    navigation : {
-                        activeColor : Constants.colors.istacBlueMedium,
-                        animation : true,
-                        arrowSize : 12,
-                        inactiveColor : Constants.colors.istacGreyMedium,
-                        style : {
-                            fontWeight : 'bold',
-                            color : Constants.colors.istacBlack,
-                            fontSize : '12px'
+                    floating: false,
+                    navigation: {
+                        activeColor: Constants.colors.istacBlueMedium,
+                        animation: true,
+                        arrowSize: 12,
+                        inactiveColor: Constants.colors.istacGreyMedium,
+                        style: {
+                            fontWeight: 'bold',
+                            color: Constants.colors.istacBlack,
+                            fontSize: '12px'
                         }
                     }
                 },
-                exporting : {
-                    enabled : false
+                exporting: {
+                    enabled: false
                 }
             };
         },
 
-        updateTitle : function () {
+        updateTitle: function () {
             var title = this.getTitle();
             this.$title.text(title);
             if (title) {
@@ -100,7 +102,7 @@
             }
         },
 
-        replaceSeries : function (chart, series) {
+        replaceSeries: function (chart, series) {
             while (chart.series.length > 0) {
                 chart.series[0].remove(false);
             }
@@ -109,107 +111,107 @@
             });
         },
 
-        setEl : function (el) {
+        setEl: function (el) {
             this.$el = $(el);
             this.el = this.$el[0];
         },
 
-        _forceDimensionTypeInZone : function(dimensionType, zone) {
-            var dimensions = this.filterDimensions.where({type : dimensionType});
+        _forceDimensionTypeInZone: function (dimensionType, zone) {
+            var dimensions = this.filterDimensions.where({ type: dimensionType });
             if (dimensions.length != 0) {
                 var dimension = dimensions[0];
-                this.filterDimensions.zones.setDimensionZone(zone, dimension, { force : true });
+                this.filterDimensions.zones.setDimensionZone(zone, dimension, { force: true });
             }
         },
 
-        _moveAllDimensionsToZone : function(zone) {
+        _moveAllDimensionsToZone: function (zone) {
             var self = this;
-            this.filterDimensions.each(function(dimension) {
-                self.filterDimensions.zones.setDimensionZone(zone, dimension, { force : true });
+            this.filterDimensions.each(function (dimension) {
+                self.filterDimensions.zones.setDimensionZone(zone, dimension, { force: true });
             });
         },
 
-        _forceMeasureDimensionInZone : function(zone) {
+        _forceMeasureDimensionInZone: function (zone) {
             this._forceDimensionTypeInZone("MEASURE_DIMENSION", zone);
         },
 
-        _forceTimeDimensionInZone : function(zone) {
+        _forceTimeDimensionInZone: function (zone) {
             this._forceDimensionTypeInZone("TIME_DIMENSION", zone);
         },
 
-        _forceGeographicDimensionInZone : function(zone) {
+        _forceGeographicDimensionInZone: function (zone) {
             this._forceDimensionTypeInZone("GEOGRAPHIC_DIMENSION", zone);
         },
 
-        _preselectBiggestHierarchyGeographicValue : function() {
+        _preselectBiggestHierarchyGeographicValue: function () {
             var fixedGeographicDimensions = this.filterDimensions.getAllFixedDimensionsCopyByType("GEOGRAPHIC_DIMENSION");
-            _(fixedGeographicDimensions).each(function(geographicDimension) { 
-                var selectedRepresentations = geographicDimension.get('representations').getSelectedRepresentations();              
-                var biggestHierarchyGeographicValue = _(selectedRepresentations).min(function(representation) {
+            _(fixedGeographicDimensions).each(function (geographicDimension) {
+                var selectedRepresentations = geographicDimension.get('representations').getSelectedRepresentations();
+                var biggestHierarchyGeographicValue = _(selectedRepresentations).min(function (representation) {
                     return representation.get("level");
                 });
                 if (biggestHierarchyGeographicValue != Infinity) {
-                    biggestHierarchyGeographicValue.set({drawable : true});
+                    biggestHierarchyGeographicValue.set({ drawable: true });
                 }
             });
         },
 
-        _preselectBiggestHierarchyGeographicValue : function() {
+        _preselectBiggestHierarchyGeographicValue: function () {
             var fixedGeographicDimensions = this.filterDimensions.getAllFixedDimensionsCopyByType("GEOGRAPHIC_DIMENSION");
-            _(fixedGeographicDimensions).each(function(geographicDimension) { 
-                var selectedRepresentations = geographicDimension.get('representations').getSelectedRepresentations();              
-                var biggestHierarchyGeographicValue = _(selectedRepresentations).min(function(representation) {
+            _(fixedGeographicDimensions).each(function (geographicDimension) {
+                var selectedRepresentations = geographicDimension.get('representations').getSelectedRepresentations();
+                var biggestHierarchyGeographicValue = _(selectedRepresentations).min(function (representation) {
                     return representation.get("level");
                 });
                 if (biggestHierarchyGeographicValue != Infinity) {
-                    biggestHierarchyGeographicValue.set({drawable : true});
+                    biggestHierarchyGeographicValue.set({ drawable: true });
                 }
             });
         },
-        
-        _preselectMostRecentTimeRepresentation : function() {
+
+        _preselectMostRecentTimeRepresentation: function () {
             var fixedTimeDimensions = this.filterDimensions.getAllFixedDimensionsCopyByType("TIME_DIMENSION");
-            _(fixedTimeDimensions).each(function(timeDimension) { 
-                var selectedRepresentations = timeDimension.get('representations').getSelectedRepresentations();              
-                var mostRecentTimeRepresentation  = timeDimension.get('reversed') 
+            _(fixedTimeDimensions).each(function (timeDimension) {
+                var selectedRepresentations = timeDimension.get('representations').getSelectedRepresentations();
+                var mostRecentTimeRepresentation = timeDimension.get('reversed')
                     ? _(selectedRepresentations).last()
                     : _(selectedRepresentations).first();
-                mostRecentTimeRepresentation.set({drawable : true});
+                mostRecentTimeRepresentation.set({ drawable: true });
             });
         },
 
-        _preselectMostPopulatedTemporalGranularityRepresentations : function() {
+        _preselectMostPopulatedTemporalGranularityRepresentations: function () {
             var fixedTimeDimensions = this.filterDimensions.getAllNonFixedDimensionsCopyByType("TIME_DIMENSION");
-            _(fixedTimeDimensions).each(function(timeDimension) { 
+            _(fixedTimeDimensions).each(function (timeDimension) {
                 var mostPopulatedTemporalGranularity = timeDimension.get('representations').getMostPopulatedTemporalGranularity();
                 timeDimension.get('representations').updateDrawablesBySelectedGranularity(mostPopulatedTemporalGranularity);
             });
         },
 
-        _preselectMostPopulatedGeographicLevelRepresentations : function() {
+        _preselectMostPopulatedGeographicLevelRepresentations: function () {
             var nonFixedGeographicDimensions = this.filterDimensions.getAllNonFixedDimensionsCopyByType("GEOGRAPHIC_DIMENSION");
-            _(nonFixedGeographicDimensions).each(function(geographicDimension) { 
+            _(nonFixedGeographicDimensions).each(function (geographicDimension) {
                 var mostPopulatedLevel = geographicDimension.get('representations').getMostPopulatedGeographicLevel();
                 geographicDimension.get('representations').updateDrawablesBySelectedLevel(mostPopulatedLevel);
             });
         },
 
-        resetDimensionsLimits : function() {
-            this.filterDimensions.zones.each(function(zone) {
+        resetDimensionsLimits: function () {
+            this.filterDimensions.zones.each(function (zone) {
                 zone.unset("fixedSize");
                 zone.unset("maxSize");
             });
         },
 
-        getRightsHolderText : function() {
+        getRightsHolderText: function () {
             return App.config.showRightsHolder && this.dataset && this.dataset.metadata.getRightsHolder() ? this.dataset.metadata.getRightsHolder().name : '';
         },
 
-        getDrawableRepresentations : function(dimension) {
+        getDrawableRepresentations: function (dimension) {
             return dimension.getDrawableRepresentations();
         },
 
-        getRightsHolderHeight : function() {
+        getRightsHolderHeight: function () {
             return 20;
         }
 
