68d50a8da37d934721674a2bbfff0ac48c0bf5cd
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2017-06-13 15:41:01 +0100

    METAMAC-2573 - Comprobar funcionamiento de las funcionalidades de compartir, descargar y widget > Autoformatting

--- metamac-portal-web-client/src/main/js/metamac/modules/svg/Exporter.js
+++ metamac-portal-web-client/src/main/js/metamac/modules/svg/Exporter.js
@@ -8,7 +8,7 @@
 
     App.svg.Exporter.prototype = {
 
-        sanitizeSvg : function (svg) {
+        sanitizeSvg: function (svg) {
             // sanitize
             svg = svg
                 .replace(/zIndex="[^"]+"/g, '')
@@ -26,7 +26,7 @@
             return svg;
         },
 
-        submitInDynamicForm : function (options) {
+        submitInDynamicForm: function (options) {
             var $form = $('<form action="' + options.url + '" method="POST">');
             _.each(options.data, function (value, key) {
                 var $input = $('<input type="hidden">');
@@ -37,16 +37,16 @@
             $form.submit();
         },
 
-        _insertStyleInSvg : function (svg, css) {
+        _insertStyleInSvg: function (svg, css) {
             var firstCloseIndex = svg.indexOf(">") + 1;
             var style = '<defs><style type="text/css"><![CDATA[' + css + ']]></style></defs>';
             return svg.substring(0, firstCloseIndex) + style + svg.substring(firstCloseIndex);
         },
 
-        addStyleAsync : function (svg) {
+        addStyleAsync: function (svg) {
             var self = this;
             var response = new $.Deferred();
-			var request = $.get(App.endpoints["statistical-visualizer"] + "/client/map.css");
+            var request = $.get(App.endpoints["statistical-visualizer"] + "/client/map.css");
             $.when(request).done(function (css) {
                 var stiledSvg = self._insertStyleInSvg(svg, css);
                 response.resolveWith(null, [stiledSvg]);
@@ -54,7 +54,7 @@
             return response.promise();
         },
 
-        mimeTypeFromType : function (type) {
+        mimeTypeFromType: function (type) {
             var mime;
             if (type === "svg") {
                 mime = "image/svg+xml";
@@ -66,25 +66,25 @@
             return mime;
         },
 
-        sanitizeSvgElement : function (svgEl) {
+        sanitizeSvgElement: function (svgEl) {
             var svgContent = svgEl[0].outerHTML;
             return this.sanitizeSvg(svgContent);
         },
 
-		// Deprecated
-        processSvgElement : function (svgEl, type) {
+        // Deprecated
+        processSvgElement: function (svgEl, type) {
             var sanitizedSvg = this.sanitizeSvgElement(svgEl);
 
             var self = this;
             this.addStyleAsync(sanitizedSvg).done(function (svg) {
                 self.submitInDynamicForm({
-                    url : App.context + "/chart/export",
-                    data : {
-                        filename : 'chart',
-                        type : self.mimeTypeFromType(type),
-                        width : $el.width(),
-                        scale : 2,
-                        svg : svg
+                    url: App.context + "/chart/export",
+                    data: {
+                        filename: 'chart',
+                        type: self.mimeTypeFromType(type),
+                        width: $el.width(),
+                        scale: 2,
+                        svg: svg
                     }
                 });
             });
