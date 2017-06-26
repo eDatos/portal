ef3fc1becb3a1e5a3c393ad07ce2f6a037cf1d10
Zebensui Méndez Corujo <zmencor@arte-consultores.com>
2017-06-26 15:06:40 +0100

    METAMAC-2621 - Separar API del Portal y visualizador en dos aplicaciones web diferentes
    - Fix recaptcha

---
+++ metamac-portal-api-web/src/main/java/org/siemac/metamac/portal/web/diffusion/filter/CookieRewritePathFilter.java
@@ -0,0 +1,34 @@
+package org.siemac.metamac.portal.web.diffusion.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CookieRewritePathFilter implements Filter {
+
+    private static final String JSESSIONID  = "JSESSIONID";
+    private static final String COOKIE_PATH = "/";
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String jSessionId = ((HttpServletRequest) request).getSession().getId();
+        ((HttpServletResponse) response).setHeader("Set-Cookie", JSESSIONID + "=" + jSessionId + ";Path=" + COOKIE_PATH);
+        chain.doFilter(request, response);
+    }
+
+}
