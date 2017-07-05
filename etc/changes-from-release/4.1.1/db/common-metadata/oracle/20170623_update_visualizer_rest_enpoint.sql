ce6b0f57bacffe66b6eec42086b29e2420c44c23
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2017-07-05 10:12:02 +0100

    METAMAC-2621 - Separar API del Portal y visualizador en dos aplicaciones web diferentes > Añadidas properties nuevas metamac.portal.web.external.visualizer y metamac.portal.web.internal.visualizer que contendrán el valor de las antiguas metamac.portal.web.external y metamac.portal.web.internal que ahora contendrán la base del visualizador.

--- etc/changes-from-release/4.1.1/db/common-metadata/oracle/20170623_update_visualizer_rest_enpoint.sql
+++ etc/changes-from-release/4.1.1/db/common-metadata/oracle/20170623_update_visualizer_rest_enpoint.sql
@@ -1,1 +1,27 @@
-UPDATE "TB_DATA_CONFIGURATIONS" SET CONF_VALUE = 'FILL_ME' WHERE CONF_KEY EQ 'metamac.portal.rest.external.base';
+------------------------------------------------------------------------------------------
+-- METAMAC-2621 Separar API del Portal y visualizador en dos aplicaciones web diferentes
+------------------------------------------------------------------------------------------
+
+UPDATE TB_DATA_CONFIGURATIONS SET CONF_VALUE = 'FILL_ME' WHERE CONF_KEY EQ 'metamac.portal.rest.external.base';
+UPDATE TB_DATA_CONFIGURATIONS SET CONF_VALUE = 'FILL_ME' WHERE CONF_KEY EQ 'metamac.portal.web.external';
+UPDATE TB_DATA_CONFIGURATIONS SET CONF_VALUE = 'FILL_ME' WHERE CONF_KEY EQ 'metamac.portal.web.internal';
+
+insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values (GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'),1,1,'metamac.portal.web.external.visualizer', 'http://FILL_ME');
+UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
+
+insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values (GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'),1,1,'metamac.portal.web.internal.visualizer', 'http://FILL_ME');
+UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
+
+-- Valores de ejemplo/demos
+
+-- UPDATE TB_DATA_CONFIGURATIONS SET CONF_VALUE = 'http://estadisticas.arte-consultores.com/statistical-visualizer-api' WHERE CONF_KEY EQ 'metamac.portal.rest.external.base';
+-- UPDATE TB_DATA_CONFIGURATIONS SET CONF_VALUE = 'http://estadisticas.arte-consultores.com/statistical-visualizer' WHERE CONF_KEY EQ 'metamac.portal.web.external';
+-- UPDATE TB_DATA_CONFIGURATIONS SET CONF_VALUE = 'http://estadisticas.arte-consultores.com/statistical-visualizer-internal' WHERE CONF_KEY EQ 'metamac.portal.web.internal';
+
+-- insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values (GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'),1,1,'metamac.portal.web.external.visualizer', 'http://estadisticas.arte-consultores.com/statistical-visualizer/visualizer');
+-- UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
+
+-- insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values (GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'),1,1,'metamac.portal.web.internal.visualizer', 'http://estadisticas.arte-consultores.com/statistical-visualizer-internal/visualizer');
+-- UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
+
+COMMIT;
