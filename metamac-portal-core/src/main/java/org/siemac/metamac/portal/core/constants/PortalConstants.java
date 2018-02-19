1fdfbd5cc81338f17188c515558f4f5a577f4971
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2018-02-19 10:04:31 +0000

    METAMAC-2693 - Implementar exportación de indicadores e instancias de indicadores > Autoformato

--- metamac-portal-core/src/main/java/org/siemac/metamac/portal/core/constants/PortalConstants.java
+++ metamac-portal-core/src/main/java/org/siemac/metamac/portal/core/constants/PortalConstants.java
@@ -2,16 +2,16 @@ package org.siemac.metamac.portal.core.constants;
 
 public class PortalConstants {
 
-    public final static String QUOTE                   = "\"";
-    public final static String SPACE                   = " ";
-    public final static String COMMA                   = ",";
-    public final static String SEMICOLON               = ";";
-    public final static String EQUALS                  = "=";
-    public final static String LEFT_PARENTHESES        = "(";
-    public final static String RIGHT_PARENTHESES       = ")";
-    public final static String NEW_LINE                = "\n";
-    public final static String LEFT_BRACE              = "[";
-    public final static String RIGHT_BRACE             = "]";
+    public final static String QUOTE             = "\"";
+    public final static String SPACE             = " ";
+    public final static String COMMA             = ",";
+    public final static String SEMICOLON         = ";";
+    public final static String EQUALS            = "=";
+    public final static String LEFT_PARENTHESES  = "(";
+    public final static String RIGHT_PARENTHESES = ")";
+    public final static String NEW_LINE          = "\n";
+    public final static String LEFT_BRACE        = "[";
+    public final static String RIGHT_BRACE       = "]";
 
     public static enum ResourceType {
         DATASET("dataset"), INDICATOR("indicator"), INDICATOR_INSTANCE("indicatorInstance");
