526281c3e5b99ce0a03a255a9ff27cef7d9e0aed
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2018-05-11 15:09:44 +0100

    METAMAC-2721 - Adaptar el visualizador para visualizar multidatasets > Añadir el cajetín de búsqueda al listado de multidatasets (al igual que se puede buscar en el resto de dimensiones).

--- metamac-portal-web-client/src/main/js/metamac/templates/HandlebarsTemplates.js
+++ metamac-portal-web-client/src/main/js/metamac/templates/HandlebarsTemplates.js
@@ -1922,10 +1922,34 @@ function program5(depth0,data) {
   return buffer;
   });
 
+this["Handlebars"]["templates"]["dataset/filter/sidebar/filter-sidebar-multidataset-node"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  var buffer = "", stack1, stack2, functionType="function", escapeExpression=this.escapeExpression;
+
+
+  buffer += "<a href=\""
+    + escapeExpression(((stack1 = ((stack1 = depth0.multidatasetNode),stack1 == null || stack1 === false ? stack1 : stack1.url)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "\" title=\"";
+  if (stack2 = helpers.title) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
+  else { stack2 = depth0.title; stack2 = typeof stack2 === functionType ? stack2.apply(depth0) : stack2; }
+  buffer += escapeExpression(stack2)
+    + "\">\r\n    <div class=\"filter-sidebar-category-left-actions\">\r\n        <i class=\"category-state  ";
+  if (stack2 = helpers.stateClass) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
+  else { stack2 = depth0.stateClass; stack2 = typeof stack2 === functionType ? stack2.apply(depth0) : stack2; }
+  buffer += escapeExpression(stack2)
+    + " level0\"></i>\r\n    </div>\r\n    <span class=\"filter-sidebar-category-label\">\r\n        ";
+  if (stack2 = helpers.label) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
+  else { stack2 = depth0.label; stack2 = typeof stack2 === functionType ? stack2.apply(depth0) : stack2; }
+  buffer += escapeExpression(stack2)
+    + "\r\n    </span>\r\n</a>";
+  return buffer;
+  });
+
 this["Handlebars"]["templates"]["dataset/filter/sidebar/filter-sidebar-multidataset"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  var buffer = "", stack1, stack2, functionType="function", escapeExpression=this.escapeExpression, self=this;
+  var buffer = "", stack1, stack2, self=this, functionType="function", escapeExpression=this.escapeExpression;
 
 function program1(depth0,data) {
   
@@ -1939,42 +1963,6 @@ function program3(depth0,data) {
   return " in ";
   }
 
-function program5(depth0,data) {
-  
-  var buffer = "", stack1;
-  buffer += "\r\n            <div class=\"filter-sidebar-category\">\r\n                <a href=\"";
-  if (stack1 = helpers.url) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = depth0.url; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "\" title=\"";
-  if (stack1 = helpers.name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = depth0.name; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "\">\r\n                    <div class=\"filter-sidebar-category-left-actions\">\r\n                        <i class=\"category-state ";
-  stack1 = helpers['if'].call(depth0, depth0.isCurrentResource, {hash:{},inverse:self.noop,fn:self.program(6, program6, data),data:data});
-  if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "  ";
-  stack1 = helpers.unless.call(depth0, depth0.isCurrentResource, {hash:{},inverse:self.noop,fn:self.program(8, program8, data),data:data});
-  if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += " level0\"></i>\r\n                    </div>\r\n                    <span class=\"filter-sidebar-category-label\">\r\n                        ";
-  if (stack1 = helpers.name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
-  else { stack1 = depth0.name; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "\r\n                    </span>\r\n                </a>\r\n            </div>\r\n            ";
-  return buffer;
-  }
-function program6(depth0,data) {
-  
-  
-  return " filter-sidebar-category-icon-radio-check ";
-  }
-
-function program8(depth0,data) {
-  
-  
-  return " filter-sidebar-category-icon-radio-unchecked ";
-  }
-
   buffer += "<div class=\"filter-sidebar-multidataset\">\r\n    <div class=\"filter-sidebar-multidataset-title ";
   stack2 = helpers['if'].call(depth0, ((stack1 = depth0.multidataset),stack1 == null || stack1 === false ? stack1 : stack1.open), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack2 || stack2 === 0) { buffer += stack2; }
@@ -1983,10 +1971,7 @@ function program8(depth0,data) {
     + "\r\n        </div>\r\n    </div>\r\n\r\n    <div class=\"filter-sidebar-multidataset-content collapse ";
   stack2 = helpers['if'].call(depth0, ((stack1 = depth0.multidataset),stack1 == null || stack1 === false ? stack1 : stack1.open), {hash:{},inverse:self.noop,fn:self.program(3, program3, data),data:data});
   if(stack2 || stack2 === 0) { buffer += stack2; }
-  buffer += "\">\r\n        <div class=\"filter-sidebar-categories\">\r\n            ";
-  stack2 = helpers.each.call(depth0, depth0.nodes, {hash:{},inverse:self.noop,fn:self.program(5, program5, data),data:data});
-  if(stack2 || stack2 === 0) { buffer += stack2; }
-  buffer += "\r\n        </div>\r\n    </div>\r\n\r\n</div>";
+  buffer += "\">\r\n        <div class=\"filter-sidebar-multidataset-searchbar\"></div>\r\n\r\n        <div class=\"filter-sidebar-multidataset-nodes\">\r\n        </div>\r\n    </div>\r\n\r\n</div>";
   return buffer;
   });
 
