706488da54ec5ad805a6a66b24e12ccd4ede0649
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2018-07-31 10:18:35 +0100

    METAMAC-2786 - No se muestran todas las notas a nivel de dimensión > Autoformato

--- metamac-portal-web-client/src/main/js/metamac/templates/HandlebarsTemplates.js
+++ metamac-portal-web-client/src/main/js/metamac/templates/HandlebarsTemplates.js
@@ -903,7 +903,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 function program1(depth0,data) {
   
   var buffer = "", stack1;
-  buffer += "\r\n<br>\r\n<div>\r\n    Notas de la categoría\r\n    <ul>\r\n    ";
+  buffer += "\r\n<br>\r\n<div>\r\n    Notas de la categoría\r\n    <ul>\r\n        ";
   stack1 = helpers.each.call(depth0, depth0.attributes, {hash:{},inverse:self.noop,fn:self.program(2, program2, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
   buffer += "\r\n    </ul>\r\n</div>\r\n";
@@ -912,19 +912,19 @@ function program1(depth0,data) {
 function program2(depth0,data) {
   
   var buffer = "", stack1;
-  buffer += "        \r\n        <li>\r\n            ";
+  buffer += "\r\n        <li>\r\n            ";
   stack1 = helpers['if'].call(depth0, depth0.href, {hash:{},inverse:self.noop,fn:self.program(3, program3, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\r\n            ";
+  buffer += " ";
   stack1 = helpers.unless.call(depth0, depth0.href, {hash:{},inverse:self.noop,fn:self.program(5, program5, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\r\n        </li>\r\n    ";
+  buffer += "\r\n        </li>\r\n        ";
   return buffer;
   }
 function program3(depth0,data) {
   
   var buffer = "", stack1;
-  buffer += "\r\n                <a href=\"";
+  buffer += "\r\n            <a href=\"";
   if (stack1 = helpers.href) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.href; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
@@ -939,9 +939,9 @@ function program3(depth0,data) {
 function program5(depth0,data) {
   
   var buffer = "";
-  buffer += "\r\n                "
+  buffer += " "
     + escapeExpression((typeof depth0 === functionType ? depth0.apply(depth0) : depth0))
-    + "\r\n            ";
+    + " ";
   return buffer;
   }
 
