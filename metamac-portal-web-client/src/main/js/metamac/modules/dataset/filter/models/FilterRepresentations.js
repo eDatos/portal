7621c698a68a5cb42c5b1d89b5c694699803be86
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2018-08-23 08:26:34 +0100

    METAMAC-2803 - Mejorar el rendimiento del visualizador para el dataset con distritos y secciones >  Para evitar posibles condiciones de carrera en las que se quedan marcados elementos que habian sido desmarcados previamente, se procede a setear todos los drawable a false antes de dibujar los nuevos

--- metamac-portal-web-client/src/main/js/metamac/modules/dataset/filter/models/FilterRepresentations.js
+++ metamac-portal-web-client/src/main/js/metamac/modules/dataset/filter/models/FilterRepresentations.js
@@ -37,10 +37,9 @@
         },
 
         _updateDrawables: function () {
-            var modelsToUndraw = this.where({ drawable: true });
             var modelsToDraw = this._getModelsToDraw();
 
-            _.invoke(modelsToUndraw, 'set', { drawable: false }, { silent: true });
+            _.invoke(this.models, 'set', { drawable: false }, { silent: true });
             _.invoke(modelsToDraw, 'set', { drawable: true });
         },
 
