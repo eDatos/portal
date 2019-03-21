084dc0fbe4a8c2f7e1c667ea3c714a033902df5f
Javier Villamonte <jvilper@arte-consultores.com>
2019-03-21 11:08:55 +0000

    METAMAC-2824 - Error al mostrar los mapas de coropletas usando los nuevos shapes (nueva proyección)
    >Cuando se ocultaba el mensaje "Debe seleccionar al menos una categoría en cada dimensión" se perdía el título de la gráfica.

--- metamac-portal-web-client/src/main/js/metamac/modules/dataset/visual-element/Base.js
+++ metamac-portal-web-client/src/main/js/metamac/modules/dataset/visual-element/Base.js
@@ -286,6 +286,7 @@
             if (this.allDimensionsHaveSelections()) {
                 if (this.$noSelection) {
                     this.$noSelection.hide();
+                    this.showTitle();
                 }
                 return true;
             } else {
