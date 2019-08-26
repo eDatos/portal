2928b6bfebd090c8278562d74fb77ff7f58e61ab
amonfue <amonfue@arte-consultores.com>
2019-08-26 13:06:41 +0100

    METAMAC-2975 - Al cambiar de dataset en multidataset no se respeta la... ...pantalla completa
    > Se borra código no específico a metamac-portal.
    
    > Se quita la clase CSS no usada 'filter-ignore-values-check'.

--- metamac-portal-web-client/src/main/js/metamac/modules/dataset/filter/sidebar/FilterSidebarMultidatasetNodeView.js
+++ metamac-portal-web-client/src/main/js/metamac/modules/dataset/filter/sidebar/FilterSidebarMultidatasetNodeView.js
@@ -16,24 +16,20 @@
 
         onMultidatasetClick: function (event) {
             var target = event.currentTarget;
-            var href = $(target).attr('href');
-            if (href && href[0] != '#') {
-                var target = event.currentTarget;
-                var newPath = target.pathname + target.search + target.hash;
-                
-                // Cambia la URL sin realizar ninguna petición
-                window.history.pushState("", "", newPath);
-    
-                var queryParams = QueryParamsUtils.getQueryParamsFromQuery(target.search);
-                queryParams.identifier = queryParams.resourceId;
-                delete queryParams.resourceId;
-                _.extend(App.queryParams, queryParams);
-    
-                // Refresca la ruta actual: https://stackoverflow.com/a/25777550
-                Backbone.history.loadUrl(Backbone.history.fragment);
-                
-                return false;
-            }
+            var newPath = target.pathname + target.search + target.hash;
+            
+            // Cambia la URL sin realizar ninguna petición
+            window.history.pushState("", "", newPath);
+
+            var queryParams = QueryParamsUtils.getQueryParamsFromQuery(target.search);
+            queryParams.identifier = queryParams.resourceId;
+            delete queryParams.resourceId;
+            _.extend(App.queryParams, queryParams);
+
+            // Refresca la ruta actual: https://stackoverflow.com/a/25777550
+            Backbone.history.loadUrl(Backbone.history.fragment);
+            
+            return false;
         },
 
         initialize: function (options) {
