f5d30bc9544b679907f42c3e7ae298f3573a1caf
amonfue <amonfue@arte-consultores.com>
2019-09-11 15:45:41 +0100

    METAMAC-2609 - Mejoras sobre los selectores de jerarquia de la barra... ...lateral
    > Se añaden granularidades a jerarquizar.

--- metamac-portal-web-client/src/main/js/metamac/modules/dataset/utils/TemporalUtils.js
+++ metamac-portal-web-client/src/main/js/metamac/modules/dataset/utils/TemporalUtils.js
@@ -7,39 +7,107 @@
 
         intervalDateParsers: {
             YEARLY: function(stringDate) {
-                var momentDate = moment(stringDate, "YYYY");
+                var momentDate = moment(stringDate, "YYYY").utc(); // parse format YYYY-A1 too
                 return {
-                    begin: momentDate.startOf('year').utc().valueOf(),
-                    end: momentDate.endOf('year').utc().valueOf()
+                    begin: momentDate.startOf('year').valueOf(),
+                    end: momentDate.endOf('year').valueOf()
                 }
             },
-            MONTHLY: function(stringDate) { // 2018-M10
-                var momentDate = moment(stringDate, "YYYY-'M'MM");
+            BIYEARLY: function(stringDate) {
+                var monthEnd = moment(stringDate, "YYYY-'S'M");
+                monthEnd.month(((monthEnd.month() + 1) * 6) - 1);
+
+                var monthBegin = monthEnd.clone();
+                monthBegin.month(monthEnd.month() - 5);
                 return {
-                    begin: momentDate.startOf('month').utc().valueOf(),
-                    end: momentDate.endOf('month').utc().valueOf()
+                    begin: monthBegin.startOf('month').utc().valueOf(),
+                    end: monthEnd.endOf('month').utc().valueOf()
+                }
+            },
+            FOUR_MONTHLY: function(stringDate) {
+                var monthEnd = moment(stringDate, "YYYY-'T'M");
+                monthEnd.month(((monthEnd.month() + 1) * 4) - 1);
+
+                var monthBegin = monthEnd.clone();
+                monthBegin.month(monthEnd.month() - 3);
+                return {
+                    begin: monthBegin.startOf('month').utc().valueOf(),
+                    end: monthEnd.endOf('month').utc().valueOf()
+                }
+            },
+            QUARTERLY: function(stringDate) {
+                var monthEnd = moment(stringDate, "YYYY-'Q'M");
+                monthEnd.month(((monthEnd.month() + 1) * 3) - 1);
+
+                var monthBegin = monthEnd.clone();
+                monthBegin.month(monthEnd.month() - 2);
+                return {
+                    begin: monthBegin.startOf('month').utc().valueOf(),
+                    end: monthEnd.endOf('month').utc().valueOf()
+                }
+            },
+            MONTHLY: function(stringDate) { // 2018-M10, 2018-10
+                var momentDate = moment(stringDate, (/^\d{4}-\d{2}$/).test(stringDate) ? 'YYYY-MM' : "YYYY-'M'MM").utc();
+                return {
+                    begin: momentDate.startOf('month').valueOf(),
+                    end: momentDate.endOf('month').valueOf()
                 }
             },
             WEEKLY: function(stringDate) { // 2018-W10
-                var momentDate = moment(stringDate, "YYYY-'W'WW");
+                var momentDate = moment(stringDate, "YYYY-'W'WW").utc();
                 return {
-                    begin: momentDate.startOf('week').utc().valueOf(),
-                    end: momentDate.endOf('week').utc().valueOf()
+                    begin: momentDate.startOf('week').valueOf(),
+                    end: momentDate.endOf('week').valueOf()
+                }
+            },
+            DAILY: function(stringDate) {
+                var matchs = stringDate.match(/^\d{4}-D(\d{1,3})$/)
+                var momentDate;
+                if (matchs) {
+                    momentDate = moment(stringDate, "YYYY");
+                    momentDate.dayOfYear(matchs[1]);
+                }
+                else {
+                    momentDate = moment(stringDate, "YYYY-MM-DD");
+                }
+                momentDate.utc();
+                return {
+                    begin: momentDate.startOf('day').valueOf(),
+                    end: momentDate.endOf('day').valueOf()
+                }
+            },
+            HOURLY: function(stringDate) {
+                var momentDate = moment(stringDate);
+                momentDate.utc();
+                return {
+                    begin: momentDate.startOf('hour').valueOf(),
+                    end: momentDate.endOf('hour').valueOf()
                 }
             }
         },
 
         temporalGranularityPriorities: {
-            MINUTELY: 0,
-            HOURLY: 1,
-            DAILY_D: 2,
-            DAILY_B: 2,
-            WEEKLY: 3,
-            MONTHLY: 4,
-            YEARLY: 8, // [YEARLY, EVENT]
+            // EVENT: 8,
+            YEARLY: 8, // 2000, 2000-A1
+            BIYEARLY: 7, // 2000-S1
+            FOUR_MONTHLY: 6, // 4 months // 2000-T3
+            QUARTERLY: 5, // 3 months // 2000-Q4
+            MONTHLY: 4, // 2000-M12, 2000-01
+            WEEKLY: 3, // 2000-W52
+            //DAILY_B: 2,
+            //DAILY_D: 2,
+            DAILY: 2, // 2000-D353, 2000-01-31
+            HOURLY: 1 // 2013-07-24T13:21:52.519+01:00
         },
 
         contains: function(majorTemporal, minorTemporal) {
+            if (!this.intervalDateParsers.hasOwnProperty(majorTemporal.temporalGranularity) ||
+                !this.intervalDateParsers.hasOwnProperty(minorTemporal.temporalGranularity)) {
+                    console.log('Some of the next granularities do not have a parser');
+                    console.log(majorTemporal.temporalGranularity);
+                    console.log(minorTemporal.temporalGranularity);
+                    return false;
+                }
             var majorInterval = this.intervalDateParsers[majorTemporal.temporalGranularity](majorTemporal.id);
             var minorInterval = this.intervalDateParsers[minorTemporal.temporalGranularity](minorTemporal.id);
 
