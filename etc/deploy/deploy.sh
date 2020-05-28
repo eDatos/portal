33b26c512bf77b60c3fbd88067982764ed8edc9c
Miguel Jonás Bravo García <mbragar@arte-consultores.com>
2020-05-28 09:28:26 +0100

    EDATOS-3146 - Instalación del entorno de estadísticas en la nueva máquina OVH
    > Se sustituye en los scripts de despliegue la IP de la nueva máquina por el nombre de la misma

--- etc/deploy/deploy.sh
+++ etc/deploy/deploy.sh
@@ -16,10 +16,10 @@ if [ "$1" == "--no-restart" ]; then
     RESTART=0
 fi
 
-scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" -r etc/deploy deploy@192.168.10.16:$TRANSFER_PATH_EXTERNAL
-scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" metamac-portal-web/target/statistical-visualizer-*.war deploy@192.168.10.16:$TRANSFER_PATH_EXTERNAL/statistical-visualizer.war
-scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" metamac-portal-api-web/target/statistical-visualizer-api-*.war deploy@192.168.10.16:$TRANSFER_PATH_EXTERNAL/statistical-visualizer-api.war
-ssh -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" deploy@192.168.10.16 <<EOF
+scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" -r etc/deploy deploy@estadisticas.arte.internal:$TRANSFER_PATH_EXTERNAL
+scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" metamac-portal-web/target/statistical-visualizer-*.war deploy@estadisticas.arte.internal:$TRANSFER_PATH_EXTERNAL/statistical-visualizer.war
+scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" metamac-portal-api-web/target/statistical-visualizer-api-*.war deploy@estadisticas.arte.internal:$TRANSFER_PATH_EXTERNAL/statistical-visualizer-api.war
+ssh -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" deploy@estadisticas.arte.internal <<EOF
 
     chmod a+x $TRANSFER_PATH_EXTERNAL/deploy/*.sh;
     . $TRANSFER_PATH_EXTERNAL/deploy/utilities.sh
