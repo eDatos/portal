9fcd454b5ae2a09d2b725882c9e3f3ed5032e662
Miguel Jonás Bravo García <mbragar@arte-consultores.com>
2020-07-30 12:01:47 +0100

    EDATOS-3188 - Integración de portal con gitlab ci para proceso de compilado, release y despliegue
    > Configuración para la compilación, release y despliegue de la aplicación mediante Gitlab CI

--- etc/deploy/scripts/demo/external/deploy.sh
+++ etc/deploy/scripts/demo/external/deploy.sh
@@ -37,7 +37,7 @@ ssh -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" depl
     sudo rm -rf $DEPLOY_TARGET_PATH/statistical-visualizer.war
     
     # Restore Configuration
-    sudo cp $ENV_CONF/web/* $DEPLOY_TARGET_PATH/statistical-visualizer
+    sudo cp -rf $ENV_CONF/web/* $DEPLOY_TARGET_PATH/statistical-visualizer
 
     ###
     # METAMAC-PORTAL-API - External
@@ -50,7 +50,7 @@ ssh -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" depl
     sudo rm -rf $DEPLOY_TARGET_PATH/statistical-visualizer-api.war
 
     # Restore Configuration
-    sudo cp $ENV_CONF/api/* $DEPLOY_TARGET_PATH/statistical-visualizer-api
+    sudo cp -rf $ENV_CONF/api/* $DEPLOY_TARGET_PATH/statistical-visualizer-api
     
     if [ $RESTART -eq 1 ]; then
         sudo chown -R edatos-external.edatos-external /servers/edatos-external
